9bd71ea6570e150d5070f655887143e3c207213b
apiotrowski255 <awesomea900@gmail.com>
2025-04-06 21:37:45 +1000

    fixed options music volume

--- globals/music_manager.gd
+++ globals/music_manager.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 var audioStreamPlayer : AudioStreamPlayer
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	audioStreamPlayer = get_node("AudioStreamPlayer")
--- scenes/options_menu.gd
+++ scenes/options_menu.gd
@@ -1,18 +1,27 @@
 extends Node2D
 
-
-
-
+func _ready():
+	var sfx_index = AudioServer.get_bus_index("Music")
+	var start_value = AudioServer.get_bus_volume_db(sfx_index)
+	$CenterContainer/VBoxContainer/HSlider.value = start_value
+	
+	var sfx_index2 = AudioServer.get_bus_index("sfx")
+	var start_value2 = AudioServer.get_bus_volume_db(sfx_index2)
+	$CenterContainer/VBoxContainer/HSlider2.value = start_value2
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 
 func _on_h_slider_drag_ended(value_changed):
-	var sfx_index= AudioServer.get_bus_index("Music")
-	AudioServer.set_bus_volume_db(sfx_index, value_changed)
+	if value_changed == true:
+		var value = $CenterContainer/VBoxContainer/HSlider.value
+		var sfx_index= AudioServer.get_bus_index("Music")
+		AudioServer.set_bus_volume_db(sfx_index, value)
 
 
 func _on_h_slider_2_drag_ended(value_changed):
-	var sfx_index= AudioServer.get_bus_index("sfx")
-	AudioServer.set_bus_volume_db(sfx_index, value_changed)
+	if value_changed == true:
+		var value = $CenterContainer/VBoxContainer/HSlider2.value
+		var sfx_index= AudioServer.get_bus_index("sfx")
+		AudioServer.set_bus_volume_db(sfx_index, value)
