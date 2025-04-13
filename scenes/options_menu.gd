4d8326217c50a12c95aef599f0768728721189dc
Rashnain <51191602+Rashnain@users.noreply.github.com>
2025-04-13 17:34:28 +0200

    improve audio options
    - from 0 to -50 with a step of 1 (going into positives degrade sound quality)
    - can be muted, when at minimum (-50)
    - improve code (both sliders with one function)

--- scenes/options_menu.gd
+++ scenes/options_menu.gd
@@ -13,15 +13,12 @@ func _on_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 
-func _on_h_slider_drag_ended(value_changed):
-	if value_changed == true:
-		var value = $CenterContainer/VBoxContainer/HSlider.value
-		var sfx_index= AudioServer.get_bus_index("Music")
-		AudioServer.set_bus_volume_db(sfx_index, value)
+func _on_slider_value_changed(value: float, bus: StringName) -> void:
+	var bus_index = AudioServer.get_bus_index(bus)
+	if value == -50:
+		AudioServer.set_bus_mute(bus_index, true)
+	else:
+		AudioServer.set_bus_mute(bus_index, false)
+		AudioServer.set_bus_volume_db(bus_index, value)
 
 
-func _on_h_slider_2_drag_ended(value_changed):
-	if value_changed == true:
-		var value = $CenterContainer/VBoxContainer/HSlider2.value
-		var sfx_index= AudioServer.get_bus_index("sfx")
-		AudioServer.set_bus_volume_db(sfx_index, value)
